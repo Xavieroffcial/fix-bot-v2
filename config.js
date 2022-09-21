9c0349661e94e4fdbb1cb5abaa73529c3278f484
ShiroNoMercy <108806813+Xavieroffcial@users.noreply.github.com>
2022-09-21 18:47:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -119,8 +119,8 @@ global.author = '                「 FiloBotz  あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'https://telegra.ph/file/7bb5e034f9fdbc3cdde6d.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/738966fcae13903e7e851.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
 global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
